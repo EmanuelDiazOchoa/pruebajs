38908f292d1f0f13f3821fbf495fe6431977feb4
emanuel diaz ochoa <emagnr93@gmail.com>
2024-08-23 21:41:39 -0300

    js de prueba

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
+// Función para saludar al usuario
+function saludarUsuario(nombre, apellido, edad) {
+    let mensajeEdad = edad >= 18 ? "Muchas gracias por visitarnos " : "Eres menor de edad ";
+    let saludo = `Hola, ${nombre} ${apellido} !! ${mensajeEdad}.`;
+    console.log(saludo); 
+
+    
+    if (edad < 18) {
+        despedirUsuario(nombre, apellido, edad); 
+    }
+}
+
+function despedirUsuario(nombre, apellido, edad) {
+    console.log("no puedes estar aqui !!");
+
+    for (let i = edad; i < 18; i++) {
+        console.log(`Adiós, ${nombre} ${apellido}!`);
+    }
+}
+
 let nombre = prompt("Bienvenido, ingresá tu nombre");
 let apellido = prompt("Ingresá tu apellido");
 let edad = prompt("Ingresa tu edad");
 
-edad = parseInt(edad);
+edad = parseInt(edad); 
 
 if (isNaN(edad)) {
     console.log("Por favor, ingresa una edad válida.");
 } else {
-    let mensajeEdad = edad >= 18 ? "mayor de 18 años" : "menor de 18 años";
-    
-    let saludo = `Hola, ${nombre} ${apellido} !! Eres ${mensajeEdad}.`;
-    
-    console.log(saludo);
+    saludarUsuario(nombre, apellido, edad); 
 }
